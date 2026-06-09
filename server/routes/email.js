eaaccabebbf0c10093f68f7439391db80109b66f
zerosmooth <zeroSmoothgtz@gmail.com>
2026-06-09 16:16:25 +0800

    remove env from tracking

--- server/routes/email.js
+++ server/routes/email.js
@@ -10,8 +10,8 @@ module.exports = (app) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp-relay.brevo.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.BREVO_USER,
         pass: process.env.BREVO_PASS,
